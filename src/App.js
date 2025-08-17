3658421e1105b63538fd5f67360b3faab8bcc043
Bautista Simone <bsimone@itba.edu.ar>
2025-08-17 01:59:39 -0300

    This is a test

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ function App() {
         <div className="row g-3">
           <div className="col-md-3">
             <input
-              placeholder="Nombre"
+              placeholder="Esto es un test"
               className="form-control"
               value={name}
               onChange={(e) => setName(e.target.value)}
